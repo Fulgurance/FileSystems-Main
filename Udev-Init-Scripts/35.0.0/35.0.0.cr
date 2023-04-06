9448b1112a1f72029d9e104084ded16810b6206a
fulgurance <zohran.londais@gmail.com>
2023-04-06 22:29:18 +0100

    Added experimentals rules for udev/eudev

---
+++ Udev-Init-Scripts/35.0.0/35.0.0.cr
@@ -0,0 +1,9 @@
+class Target < ISM::Software
+
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
