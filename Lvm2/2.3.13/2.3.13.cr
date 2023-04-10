ce39957738947222595eb380d0895fcbf0071285
fulgurance <zohran.londais@gmail.com>
2023-04-10 12:09:12 +0100

    Added openrc support for lvm

--- Lvm2/2.3.13/2.3.13.cr
+++ Lvm2/2.3.13/2.3.13.cr
@@ -13,13 +13,39 @@ class Target < ISM::Software
     def build
         super
 
-        makeSource([Ism.settings.makeOptions],buildDirectoryPath)
+        makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
 
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        if option("Openrc")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/device-mapper.rc-2.02.105-r2","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/device-mapper")
+            runChmodCommand(["+x","device-mapper"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/dmeventd.initd-2.02.184-r2","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/dmeventd")
+            runChmodCommand(["+x","dmeventd"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/lvm-monitoring.initd-2.02.105-r2","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/lvm-monitoring")
+            runChmodCommand(["+x","lvm-monitoring"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/lvm.rc-2.02.187","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/lvm")
+            runChmodCommand(["+x","lvm"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/lvmlockd.initd-2.02.166-r1","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/lvmlockd")
+            runChmodCommand(["+x","lvmlockd"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}/lvmpolld.initd-2.02.183","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/lvmpolld")
+            runChmodCommand(["+x","lvmpolld"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/init.d")
+        end
     end
 
 end
