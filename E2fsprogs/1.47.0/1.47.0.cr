4efb7fbbce0598562489a2e0954a3df13e179da8
Fulgurance <zohran.londais@gmail.com>
2023-11-12 17:30:10 +0000

    Added new updates

---
+++ E2fsprogs/1.47.0/1.47.0.cr
@@ -0,0 +1,46 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--enable-elf-shlibs",
+                            "--disable-libblkid",
+                            "--disable-libuuid",
+                            "--disable-uuidd",
+                            "--disable-fsck"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        runGunzipCommand(["libext2fs.info.gz"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/info/")
+    end
+
+    def install
+        super
+
+        runInstallInfoCommand([ "--dir-file=/usr/share/info/dir",
+                                "/usr/share/info/libext2fs.info"])
+
+        deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib/libcom_err.a")
+        deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib/libe2p.a")
+        deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib/libext2fs.a")
+        deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib/libss.a")
+    end
+
+end
---
+++ Lvm2/2.3.22/2.3.22.cr
@@ -0,0 +1,41 @@
+class Target < ISM::Software
+
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--enable-cmdlib",
+                            "--enable-pkgconfig",
+                            "--enable-udev_sync"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        if option("Openrc")
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Device-Mapper-Init.d","device-mapper")
+
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Dmeventd-Init.d","dmeventd")
+
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Lvm-Monitoring-Init.d","lvm-monitoring")
+
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Lvm-Init.d","lvm")
+
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Lvmlockd-Init.d","lvmlockd")
+
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Lvmpolld-Init.d","lvmpolld")
+
+            deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib/udev/rules.d/69-dm-lvm.rules")
+        end
+    end
+
+end
