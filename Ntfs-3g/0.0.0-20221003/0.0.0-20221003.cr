6df53b7dc9c2402e245b6a9186b22dedf7d99d8f
Fulgurance <zohran.londais@gmail.com>
2024-05-05 14:17:53 +0100

    Updated symlink recording method

--- Ntfs-3g/0.0.0-20221003/0.0.0-20221003.cr
+++ Ntfs-3g/0.0.0-20221003/0.0.0-20221003.cr
@@ -2,6 +2,7 @@ class Target < ISM::Software
 
     def configure
         super
+
         configureSource([   "--prefix=/usr",
                             "--disable-static",
                             "--with-fuse=internal",
@@ -11,18 +12,17 @@ class Target < ISM::Software
 
     def build
         super
+
         makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
-        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-    end
 
-    def install
-        super
-        makeLink("../bin/ntfs-3g","#{Ism.settings.rootPath}usr/sbin/mount.ntfs",:symbolicLink)
-        makeLink("ntfs-3g.8","#{Ism.settings.rootPath}usr/share/man/man8/mount.ntfs.8",:symbolicLink)
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        makeLink("../bin/ntfs-3g","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/sbin/mount.ntfs",:symbolicLink)
+        makeLink("ntfs-3g.8","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/man/man8/mount.ntfs.8",:symbolicLink)
     end
 
 end
