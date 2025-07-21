660cbd1861747a500532a0080326f94bd1cc58b1
Fulgurance <zohran.londais@gmail.com>
2025-07-21 18:55:28 +0100

    Added musl support for btrfs

--- Btrfs-Progs/6.10.1/6.10.1.cr
+++ Btrfs-Progs/6.10.1/6.10.1.cr
@@ -9,19 +9,27 @@ class Target < ISM::Software
 
     def configure
         super
+
+        usingGlibc = component("C-Library").uniqueDependencyIsEnabled("Glibc")
+
+        backtrace = (usingGlibc ? "--enable-backtrace" : "--disable-backtrace")
+
         configureSource(arguments:  "--prefix=/usr              \
                                     --disable-static            \
-                                    --disable-documentation",
+                                    --disable-documentation     \
+                                    #{backtrace}",
                         path:       buildDirectoryPath)
     end
 
     def build
         super
+
         makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
+
         makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
     end
